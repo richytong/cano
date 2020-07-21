d89bdc87429b09f12580de58ff7d39d7a477112f
Richard <richytong@gmail.com>
2020-07-21 13:55:29 -0700

    cratos list, ls

--- new.js
+++ new.js
@@ -2,8 +2,13 @@
 
 const rubico = require('rubico')
 const trace = require('rubico/x/trace')
+const tracef = require('rubico/x/tracef')
+const last = require('rubico/x/last')
 const execa = require('execa')
+const fs = require('fs')
+const nodePath = require('path')
 const USAGE = require('./USAGE')
+const cratosPackageJSON = require('./package.json')
 
 const {
   pipe, fork, assign,
@@ -14,6 +19,12 @@ const {
   get, pick, omit,
 } = rubico
 
+// any => any
+const identity = x => x
+
+// string => string
+const pathResolve = nodePath.resolve
+
 // (from number, to number) => arr [any] => slicedArr [any]
 const slice = (from, to) => arr => arr.slice(
   from,
@@ -21,13 +32,33 @@ const slice = (from, to) => arr => arr.slice(
   // typeof to === 'undefined' ? undefined : to < 0 ? arr.length + to : to,
 )
 
-// prefix string => string => boolean
+// string => [string]
+const split = delim => s => s.split(delim)
+
+// [any] => string
+const join = delim => arr => arr.join(delim)
+
+// value any => someArray [any] => boolean
+const includes = value => arr => arr.includes(value)
+
+// string => string
+const lastWord = pipe([split(' '), last])
+
+// prefix string => s string => boolean
 const startsWith = prefix => s => s.startsWith(prefix)
+
+// level string => (args ...any) => ()
+const logWithLevel = level => (...args) => console.log(level, ...args)
+
+const logger = {
+  warn: logWithLevel('[WARNING]'),
+}
 
 // argv [string] => cleanedArgv [string]
 const cleanArgv = slice(2)
 
-/* argv [string] => parsedArgv {
+/*
+ * argv [string] => parsedArgv {
  *   arguments: [string],
  *   flags: [string],
  * }
@@ -40,6 +71,163 @@ const parseArgv = pipe([
   }),
 ])
 
+// parsedArgv { flags: [string] } => parsedFlags Map { parsedFlag string -> flagValue string|boolean }
+const parseFlags = ({ flags }) => transform(
+  map(pipe([
+    flag => flag.split('='),
+    fork([
+      pipe([get(0), s => s.replace(/-/g, '')]),
+      get(1, true),
+    ]),
+  ])),
+  () => new Map(),
+)(flags)
+
+/*
+ * parsedArgv {
+ *   arguments: [string],
+ *   flags: [string],
+ * } => entryPath string
+ */
+const findEntryPath = pipe([
+  parseFlags,
+  switchCase([
+    flags => flags.has('path'), flags => flags.get('path'),
+    () => 'CRATOS_PATH' in process.env, () => process.env.CRATOS_PATH,
+    () => 'HOME' in process.env, pipe([
+      tap(() => {
+        logger.warn('CRATOS_PATH not set; finding modules from HOME')
+      }),
+      () => process.env.HOME,
+    ]),
+    () => {
+      throw new Error('no entrypoint found; CRATOS_PATH or HOME environment variables required')
+    },
+  ]),
+])
+
+// Directory {
+//   path: string,
+//   dirents: [NodeDirent],
+// } => boolean
+const isCratosModule = ({ dirents }) => pipe([
+  map(get('name')),
+  and([
+    includes('.git'),
+    includes('package.json'),
+  ]),
+])(dirents)
+
+const IGNORE_DIRS = new Set(['.git', 'node_modules'])
+
+// NodeDirent => boolean
+const isIgnoreDir = dirent => IGNORE_DIRS.has(dirent.name)
+
+// path string => moduleNames [string]
+const walkPathForModuleNames = pathArg => pipe([
+  fork({
+    path: identity,
+    dirents: tryCatch(
+      path => fs.promises.readdir(path, { withFileTypes: true }),
+      () => [],
+    ),
+  }),
+  switchCase([
+    isCratosModule, ({ path }) => [path],
+    ({ path, dirents }) => transform(pipe([
+      filter(and([
+        not(isIgnoreDir),
+        dirent => dirent.isDirectory(),
+      ])),
+      flatMap(pipe([
+        get('name'),
+        dirName => pathResolve(path, dirName),
+        walkPathForModuleNames,
+      ])),
+    ]), () => [])(dirents),
+  ]),
+])(pathArg)
+
+/*
+ * parsedArgv {
+ *   arguments: [string],
+ *   flags: [string],
+ * } => modulePaths [string]
+ */
+const findModulePaths = pipe([
+  findEntryPath,
+  split(':'),
+  flatMap(pipe([
+    pathResolve,
+    walkPathForModuleNames,
+  ])),
+])
+
+/*
+ * path string => packageJSON {
+ *   name: string,
+ *   version: string,
+ * }
+ */
+const getPackageJSON = pipe([
+  path => pathResolve(path, 'package.json'),
+  fs.promises.readFile,
+  JSON.parse,
+])
+
+// path string => gitStatus object
+const getGitStatus = pipe([
+  tryCatch(
+    path => execa('git', [
+      `--git-dir=${pathResolve(path, '.git')}`,
+      `--work-tree=${path}`,
+      'status',
+      '--porcelain',
+      '--branch',
+    ]),
+    err => {
+      throw new Error(err.stderr)
+    },
+  ),
+  get('stdout'),
+  stdout => stdout.split('\n'),
+  fork({
+    branch: get(0),
+    files: slice(1),
+  }),
+  assign({
+    fileNames: ({ files }) => map(lastWord)(files),
+  }),
+])
+
+/* path string => cratosModule {
+ *   path: string,
+ *   packageName: string,
+ *   packageVersion: string,
+ *   gitCurrentBranch: [string],
+ *   gitStatusFiles: [string],
+ *   gitStatusFileNames: [string],
+ * }
+ */
+const getModuleInfo = pipe([
+  fork({
+    path: identity,
+    packageJSON: getPackageJSON,
+    gitStatus: getGitStatus,
+  }),
+  fork({
+    path: get('path'),
+    packageName: get('packageJSON.name', 'UNNAMED'),
+    packageVersion: get('packageJSON.version', '0.0.0'),
+    gitCurrentBranch: pipe([
+      get('gitStatus.branch'),
+      s => s.slice(3),
+    ]),
+    gitStatusFiles: get('gitStatus.files'),
+    gitStatusFileNames: get('gitStatus.fileNames'),
+  }),
+])
+
 // flag string => parsedArgv { flags: [string] } => boolean
 const hasFlag = flag => ({ flags }) => flags.includes(flag)
 
@@ -49,28 +237,100 @@ const isBaseCommand = ({ arguments: args }) => args.length === 0
 // string => parsedArgv { arguments: [string] } => boolean
 const isCommand = cmd => ({ arguments: args }) => args[0] === cmd
 
+// replacement string => s string => replaced string
+const replaceHomeWith = replacement => s => s.replace(process.env.HOME, replacement)
+
+const command = {
+  /*
+   * () => command {
+   *   type: 'VERSION',
+   *   body: {
+   *     version: string,
+   *   },
+   * }
+   */
+  version: pipe([
+    () => ({
+      type: 'VERSION',
+      body: {
+        version: cratosPackageJSON.version,
+      },
+    }),
+    tap(({ body: { version } }) => {
+      console.log('v' + version)
+    }),
+  ]),
+
+  /*
+   * () => command {
+   *   type: 'USAGE',
+   *   body: {},
+   * }
+   */
+  usage: () => {
+    console.log(USAGE)
+    return {
+      type: 'USAGE',
+      body: {},
+    }
+  },
+
+  /*
+   * parsedArgv {
+   *   arguments: [string],
+   *   flags: [string],
+   * } => command {
+   *   type: 'LIST',
+   *   body: {
+   *     modules: [cratosModule {
+   *       path: string,
+   *       packageName: string,
+   *       packageVersion: string,
+   *       gitCurrentBranch: [string],
+   *       gitStatusFiles: [string],
+   *       gitStatusFileNames: [string],
+   *     }]
+   *   },
+   * }
+   */
+  list: pipe([
+    findModulePaths,
+    fork({
+      type: () => 'LIST',
+      body: fork({
+        modules: map(pipe([
+          getModuleInfo,
+          tap(({ packageName, packageVersion }) => {
+            console.log(`${packageName}-${packageVersion}`)
+          }),
+        ])),
+      }),
+    }),
+  ]),
+}
+
 /*
  * argv [string] => {
  *   arguments: [string],
  *   flags: [string],
  *   command: {
  *     type: string,
+ *     body: oneOf([
+ *       versionBody { version: string },
+ *       emptyBody {},
+ *       moduleBody { modules: [cratosModule] },
+ *     ]),
  *   },
  * }
  *
- * ['node', './cli.js'] => {
+ * ['node', 'cratos', '--version'] => {
  *   arguments: [],
- *   flags: [],
+ *   flags: ['--version'],
  *   command: {
- *     type: 'USAGE',
- *   },
- * }
- *
- * ['/usr/bin/node', '/usr/bin/cratos', '--help'] => {
- *   arguments: [],
- *   flags: ['--help'],
- *   command: {
- *     type: 'USAGE',
+ *     type: 'VERSION',
+ *     body: {
+ *       version: string,
+ *     },
  *   },
  * }
  */
@@ -79,24 +339,27 @@ const cratos = pipe([
   assign({
     command: switchCase([
       or([
+        hasFlag('--version'),
+        hasFlag('-v'),
+      ]), command.version,
+      or([
         hasFlag('--help'),
         hasFlag('-h'),
         isBaseCommand,
-      ]), () => {
-        console.log(USAGE)
-        return {
-          type: 'USAGE',
-        }
-      },
+      ]), command.usage,
+      or([
+        isCommand('list'),
+        isCommand('ls'),
+      ]), command.list,
       x => {
         console.log(`${x.arguments[0]} is not a cratos command\n${USAGE}`)
         return {
           type: 'INVALID_USAGE',
+          body: {},
         }
       },
     ]),
   }),
-  trace,
 ])
 
 module.exports = cratos
--- new.test.js
+++ new.test.js
@@ -1,12 +1,99 @@
-const isDeepEqual = require('rubico/x/isDeepEqual')
+const rubico = require('rubico')
+const trace = require('rubico/x/trace')
+const tracef = require('rubico/x/tracef')
+const execa = require('execa')
 const assert = require('assert')
+const rimrafCb = require('rimraf')
+const nodePath = require('path')
+const util = require('util')
+const fs = require('fs')
+const { promisify } = require('util')
 const cratos = require('./new')
+const cratosPackageJSON = require('./package.json')
+
+const {
+  pipe, fork, assign,
+  tap, tryCatch, switchCase,
+  map, filter, reduce, transform,
+  any, all, and, or, not,
+  eq, gt, lt, gte, lte,
+  get, pick, omit,
+} = rubico
+
+const pathResolve = nodePath.resolve
+
+const rimraf = promisify(rimrafCb)
 
 const ade = assert.deepEqual
 
 const ase = assert.strictEqual
 
+const git = (args, path) => execa('git', [
+  `--git-dir=${pathResolve(path, '.git')}`,
+  `--work-tree=${path}`,
+  ...args,
+])
+
+const createProjectFixture = (path, packageJSON) => fork.series([
+  path => fs.promises.mkdir(pathResolve(path), { recursive: true }),
+  path => git(['init'], pathResolve(path)),
+  fork([
+    path => fs.promises.writeFile(
+      pathResolve(path, 'package.json'),
+      JSON.stringify(packageJSON, null, 2),
+    ),
+    path => fs.promises.writeFile(
+      pathResolve(path, 'index.js'),
+      `
+module.exports = {}
+`.trimStart(),
+    ),
+  ]),
+])(path)
+
+const createEmptyDirectory = path => fs.promises.mkdir(path, { recursive: true })
+
 describe('cratos', () => {
+  before(() => {
+    process.env.CRATOS_PATH = 'tmp'
+  })
+
+  afterEach(async () => {
+    await rimraf('tmp')
+  })
+
+  it('responds with version on --version', async () => {
+    ade(
+      cratos(['node', 'cratos', '--version']),
+      {
+        arguments: [],
+        flags: ['--version'],
+        command: {
+          type: 'VERSION',
+          body: {
+            version: cratosPackageJSON.version,
+          },
+        },
+      },
+    )
+  })
+
+  it('responds with version on -v', async () => {
+    ade(
+      cratos(['node', 'cratos', '-v']),
+      {
+        arguments: [],
+        flags: ['-v'],
+        command: {
+          type: 'VERSION',
+          body: {
+            version: cratosPackageJSON.version,
+          },
+        },
+      },
+    )
+  })
+
   it('responds with usage on base command', async () => {
     ade(
       cratos(['node', './cli.js']),
@@ -15,6 +102,7 @@ describe('cratos', () => {
         flags: [],
         command: {
           type: 'USAGE',
+          body: {},
         },
       },
     )
@@ -25,6 +113,7 @@ describe('cratos', () => {
         flags: ['--yo'],
         command: {
           type: 'USAGE',
+          body: {},
         },
       },
     )
@@ -38,6 +127,7 @@ describe('cratos', () => {
         flags: ['--help'],
         command: {
           type: 'USAGE',
+          body: {},
         },
       },
     )
@@ -51,9 +141,51 @@ describe('cratos', () => {
         flags: ['-h'],
         command: {
           type: 'USAGE',
+          body: {},
         },
       },
     )
+  })
+
+  /*
+   * string => packageJSON {
+   *   name: string,
+   *   version: string,
+   * }
+   */
+  const generatePackageJSON = s => ({
+    name: `new-project-${s}`,
+    version: `0.0.${s.charCodeAt(0)}`,
+  })
+
+  it('responds with list on list', async () => {
+    await map(
+      s => createProjectFixture(`tmp/${s}`, generatePackageJSON(s)),
+    )(['a', 'b', 'c'])
+    await pipe([
+      cratos,
+      x => {
+        ade(x.arguments, ['list'])
+        ade(x.flags, [])
+        ase(x.command.type, 'LIST')
+        ase(x.command.body.modules.length, 3)
+      },
+    ])(['/usr/bin/node', '/usr/bin/cratos', 'list'])
+  })
+
+  it('responds with list on ls', async () => {
+    await map(
+      s => createProjectFixture(`tmp/${s}`, generatePackageJSON(s)),
+    )(['a', 'b', 'c'])
+    await pipe([
+      cratos,
+      x => {
+        ade(x.arguments, ['ls'])
+        ade(x.flags, [])
+        ase(x.command.type, 'LIST')
+        ase(x.command.body.modules.length, 3)
+      },
+    ])(['/usr/bin/node', '/usr/bin/cratos', 'ls'])
   })
 
   it('responds with usage on invalid command', async () => {
@@ -64,6 +196,7 @@ describe('cratos', () => {
         flags: [],
         command: {
           type: 'INVALID_USAGE',
+          body: {},
         },
       },
     )
